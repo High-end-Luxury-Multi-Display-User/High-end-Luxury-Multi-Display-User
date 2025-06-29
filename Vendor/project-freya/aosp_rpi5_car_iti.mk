47dd04f399facc27ebedbd59b5a2c6b23fd242e9
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-06-29 18:21:40 +0300

    added makefile fragment for vendor

---
+++ Vendor/project-freya/aosp_rpi5_car_iti.mk
@@ -0,0 +1,4 @@
+PRODUCT_PACKAGES += MultiDisplayRpiOverlays AndroidRpiFrameWorkMultiDisplay MultiDisplaySecondaryHomeTestLauncher SystemUIStatusBarOnly libgpiod libgpiohalrpi5 Rpi5GPIOTest
+
+PRODUCT_COPY_FILES += vendor/iti/init.gpiochip.rc:root/init.gpiochip.rc
+
